3890b47f70f7733857025f9d12a5528cfd2faa96
zackyape <75405032+zackyape@users.noreply.github.com>
2025-11-11 23:05:58 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -95,6 +95,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Camera
+$(call inherit-product-if-exists, vendor/xiaomi/vayu-miuicamera/products/miuicamera.mk)
+
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
