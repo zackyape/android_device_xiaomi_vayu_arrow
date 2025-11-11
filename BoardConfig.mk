72a4f594ba213bcababeaa8fdefd96fd5c502540
zackyape <75405032+zackyape@users.noreply.github.com>
2025-11-12 00:20:44 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ TARGET_DISABLED_UBWC := true
 
 # Camera
 TARGET_USES_QTI_CAMERA_DEVICE := true
+MALLOC_SVELTE_FOR_LIBC32 := true
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
